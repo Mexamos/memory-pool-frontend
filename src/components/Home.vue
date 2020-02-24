089cc5c3c6b6f5be656224dfc2adbda47c4806de
Maksim <tolstikov_m@mail.ru>
2020-02-24 18:49:37 +0300

    Init app

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.hello {
+  h1 {
+    color: red;
+  }
+}
+</style>
